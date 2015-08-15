25f350b0320f250a1ac07da6263d74b354c312c8
Qawelesizwe <qawemlilo@gmail.com>
2015-08-15 08:42:04 +0200

    template update

---
+++ template/js/script.js
@@ -0,0 +1,52 @@
+;(function (window) {
+    "use strict";
+    
+    function styleRibbons() {
+        var images = document.getElementsByTagName('img'), i;
+    
+        for (i = 0; i < images.length; i++) {
+            if (images[i].title === 'Github') {
+                images[i].className = 'github';
+            }
+        }
+    }
+    
+    
+    function addClassNames() {
+        var pre = document.getElementsByTagName('pre'), i;
+    
+        for (i = 0; i < pre.length; i++) {
+            pre[i].className = "prettyprint linenums";
+        }
+    }
+    
+    
+    function inspectLinks() {
+        var links = document.getElementsByTagName('a'), 
+            i, href, isInternalLink,
+            mysite = window.location.href;
+        
+        isInternalLink = function (href, link) {
+            var hrefParser = document.createElement('a'),
+                linkParser = document.createElement('a');
+        
+            hrefParser.href = href;
+            linkParser.href = link;
+        
+            return hrefParser.hostname === linkParser.hostname;
+        };
+    
+        for (i = 0; i < links.length; i++) {
+            href = links[i].href;
+            
+            if (!isInternalLink(mysite, href)) {
+                links[i].target = '_blank';
+            }
+        }
+    }
+
+    addClassNames();
+    prettyPrint();
+    inspectLinks();
+    styleRibbons();
+}(window));
